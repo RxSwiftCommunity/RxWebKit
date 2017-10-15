c13fcad5ebdc5231cbbe47c19dc3124625c49e8c
Ryne Cheow <rynecheow@gmail.com>
2017-10-15 11:35:52 +0800

    Flatten documentation name

--- RxWebKit/Sources/Rx+WebKit.swift
+++ RxWebKit/Sources/Rx+WebKit.swift
@@ -14,48 +14,36 @@ import WebKit
 #endif
 
 extension Reactive where Base: WKWebView {
-    /**
-     Reactive wrapper for `title` property
-     */
+
+    /// Reactive wrapper for `title` property
     public var title: Observable<String?> {
         return self.observe(String.self, "title")
     }
 
-    /**
-     Reactive wrapper for `loading` property.
-     */
-    public var loading: Observable<Bool> {
+    /// Reactive wrapper for `loading` property.
+    public var isLoading: Observable<Bool> {
         return self.observe(Bool.self, "loading")
             .map { $0 ?? false }
     }
 
-    /**
-     Reactive wrapper for `estimatedProgress` property.
-     */
+    /// Reactive wrapper for `estimatedProgress` property.
     public var estimatedProgress: Observable<Double> {
         return self.observe(Double.self, "estimatedProgress")
             .map { $0 ?? 0.0 }
     }
 
-    /**
-     Reactive wrapper for `url` property.
-     */
+    /// Reactive wrapper for `url` property.
     public var url: Observable<URL?> {
         return self.observe(URL.self, "URL")
     }
 
-
-    /**
-     Reactive wrapper for `canGoBack` property.
-     */
+    /// Reactive wrapper for `canGoBack` property.
     public var canGoBack: Observable<Bool> {
         return self.observe(Bool.self, "canGoBack")
             .map { $0 ?? false }
     }
 
-    /**
-     Reactive wrapper for `canGoForward` property.
-     */
+    /// Reactive wrapper for `canGoForward` property.
     public var canGoForward: Observable<Bool> {
         return self.observe(Bool.self, "canGoForward")
             .map { $0 ?? false }
