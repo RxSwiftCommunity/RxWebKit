0ba52fa7e33a1251832dfc85a72d831c95757c95
Obi Bob Godwin <bobgodwinx@gmail.com>
2017-10-25 18:30:57 +0200

    JavaScriptConfirm example
    Introduced `JavaScriptConfirmPanelViewController` to support the confirm example event

---
+++ Example/JavaScriptConfirmPanelViewController.swift
@@ -0,0 +1,43 @@
+//
+//  JavaScriptConfirmPanelViewController.swift
+//  Example
+//
+//  Created by Bob Obi on 25.10.17.
+//  Copyright © 2017 MokuMokuCloud. All rights reserved.
+//
+
+import UIKit
+import WebKit
+import RxWebKit
+import RxSwift
+import RxCocoa
+
+class JavaScriptConfirmPanelViewController: UIViewController {
+
+    let bag = DisposeBag()
+    let wkWebView = WKWebView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(wkWebView)
+        wkWebView.load(URLRequest(url: URL(string: "https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_confirm2")!))
+        
+        wkWebView.rx
+            .javaScriptConfirmPanel
+            .debug("javaScriptConfirmPanel")
+            .subscribe(onNext: {(webView, message, frame, handler) in
+                let alert = UIAlertController(title: "JavaScriptConfirm", message: message, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                handler(true)
+            })
+            .disposed(by: bag)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let originY = UIApplication.shared.statusBarFrame.maxY
+        wkWebView.frame = CGRect(x: 0, y: originY, width: view.bounds.width, height: view.bounds.height)
+    }
+
+}
