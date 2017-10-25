fd3d3cae682da5e50ac77ce2dc016235b7d159f9
Obi Bob Godwin <bobgodwinx@gmail.com>
2017-10-25 18:09:34 +0200

    Examples updates + JavaScriptAlertPanelViewController
    Updated xcodeproject example so that storyboard will use `iPhone X` and introduced example for `JavaScriptAlertPanel`

---
+++ Example/JavaScriptAlertPanelViewController.swift
@@ -0,0 +1,42 @@
+//
+//  JavaScriptAlertPanelViewController.swift
+//  Example
+//
+//  Created by Bob Obi on 25.10.17.
+//  Copyright © 2017 MokuMokuCloud. All rights reserved.
+//
+
+import UIKit
+import WebKit
+import RxWebKit
+import RxSwift
+import RxCocoa
+
+class JavaScriptAlertPanelViewController: UIViewController {
+    
+    let bag = DisposeBag()
+    let wkWebView = WKWebView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(wkWebView)
+        wkWebView.load(URLRequest(url: URL(string: "https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_alert")!))
+        
+        wkWebView.rx
+            .javaScriptAlertPanel
+            .debug("javaScriptAlertPanel")
+            .subscribe(onNext: {(webView, message, frame, handler) in
+                let alert = UIAlertController(title: "JavaScriptAlertPanel", message: message, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                handler()
+            })
+            .disposed(by: bag)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let originY = UIApplication.shared.statusBarFrame.maxY
+        wkWebView.frame = CGRect(x: 0, y: originY, width: view.bounds.width, height: view.bounds.height)
+    }
+}
