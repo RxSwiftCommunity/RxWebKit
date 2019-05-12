a77a97a87f78478da524829d71688b0c3a7f4d69
Bob Godwin Obi <bob@BobMacBokPro18K.fritz.box>
2019-05-12 10:43:13 +0200

    Added an extension to `TestScheduler` : `func record<O: ObservableConvertibleType>(source: O) -> TestableObserver<O.Element>`

--- RxWebKitTests/HasEventsBehavior.swift
+++ RxWebKitTests/HasEventsBehavior.swift
@@ -15,3 +15,18 @@ struct HasEventsBehaviorContext<T> {
         self.observable = observable
     }
 }
+
+
+extension TestScheduler {
+    /// Builds testable observer for s specific observable sequence, binds it's results and sets up disposal.
+    /// parameter source: Observable sequence to observe.
+    /// returns: Observer that records all events for observable sequence.
+    func record<O: ObservableConvertibleType>(source: O) -> TestableObserver<O.Element> {
+        let observer = self.createObserver(O.Element.self)
+        let disposable = source.asObservable().bind(to: observer)
+        self.scheduleAt(100000) {
+            disposable.dispose()
+        }
+        return observer
+    }
+}
