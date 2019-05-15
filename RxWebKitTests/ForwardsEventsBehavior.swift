7cee9f78b67a452e490dd06db5bc01755178e0a8
Bob Godwin Obi <bob@BobMacBokPro18K.fritz.box>
2019-05-15 16:42:30 +0200

    Introduced `ForwardsEventsBehavior`

--- RxWebKitTests/ForwardsEventsBehavior.swift
+++ RxWebKitTests/ForwardsEventsBehavior.swift
@@ -21,3 +21,47 @@ struct ForwardsEventsBehaviorContext {
         self.invoked = invoked
     }
 }
+
+class ForwardsEventsBehavior: Quick.Behavior<ForwardsEventsBehaviorContext> {
+    override class func spec(_ context: @escaping () -> ForwardsEventsBehaviorContext) {
+        
+        var sut: WKWebView!
+        var scheduler: TestScheduler!
+        var selector: Selector!
+        var invoked: (() -> ())!
+        
+        beforeEach {
+            let cxt = context()
+            sut = cxt.sut
+            scheduler = cxt.scheduler
+            selector = cxt.selector
+            invoked = cxt.invoked
+        }
+        
+        afterEach {
+            sut = nil
+            scheduler = nil
+            selector = nil
+        }
+        
+        describe("Has Events Behavior") {
+            it("sentMessage") {
+                SharingScheduler.mock(scheduler: scheduler) {
+                    let sentMessage = scheduler.record(source: sut.rx.delegate.sentMessage(selector))
+                    invoked()
+                    scheduler.start()
+                    expect(sentMessage.events.count).to(equal(1))
+                }
+            }
+            
+            it("methodInvoke") {
+                SharingScheduler.mock(scheduler: scheduler) {
+                    let methodInvoked = scheduler.record(source: sut.rx.delegate.methodInvoked(selector))
+                    invoked()
+                    scheduler.start()
+                    expect(methodInvoked.events.count).to(equal(1))
+                }
+            }
+        }
+    }
+}
