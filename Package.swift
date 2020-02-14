fe96e61875a8da530aa4321801b6c8546795b7c5
Taishi Ikai <t@ikai.tokyo>
2020-02-14 14:46:27 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,13 +4,28 @@ import PackageDescription
 
 let package = Package(
     name: "RxWebKit",
+    platforms: [
+        .iOS(.v8)
+    ],
     products: [
         .library(name: "RxWebKit", targets: ["RxWebKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift", from: "5.0.1")
+        .package(url: "https://github.com/ReactiveX/RxSwift", .upToNextMajor(from: "5.0.0")),
+        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "2.2.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.0"))
     ],
     targets: [
-        .target(name: "RxWebKit", dependencies: ["RxSwift", "RxCocoa"], path: "RxWebKit")
-    ]
+        .target(
+            name: "RxWebKit",
+            dependencies: ["RxSwift", "RxCocoa", "RxRelay"],
+            path: "RxWebKit"
+        ),
+        .testTarget(
+            name: "RxWebKitTests",
+            dependencies: ["RxWebKit", "Nimble", "Quick"],
+            path: "RxWebKitTests"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
 )
