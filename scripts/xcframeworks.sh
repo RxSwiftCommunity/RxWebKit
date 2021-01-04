0b7ed21b82c06e30f763edc181da1865c4954f3c
Ryne Cheow <rynecheow@gmail.com>
2021-01-04 20:48:18 +0800

    fix: Remove unused github step

--- scripts/xcframeworks.sh
+++ scripts/xcframeworks.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 rm -rf xcarchives/*
 rm -rf RxWebKit.xcframework.zip
 
-xcodebuild archive -project RxWebKit-SPM.xcodeproj -scheme "RxWebKit iOS" -sdk iphoneos -archivePath "xcarchives/RxWebKit-iOS"
-xcodebuild archive -project RxWebKit-SPM.xcodeproj -scheme "RxWebKit iOS" -sdk iphonesimulator  -archivePath "xcarchives/RxWebKit-iOS-Simulator"
-xcodebuild archive -project RxWebKit-SPM.xcodeproj -scheme "RxWebKit macOS" -sdk macosx -archivePath "xcarchives/RxWebKit-macOS"
+xcodebuild archive -quiet -project RxWebKit-SPM.xcodeproj -scheme "RxWebKit iOS" -sdk iphoneos -archivePath "xcarchives/RxWebKit-iOS"
+xcodebuild archive -quiet -project RxWebKit-SPM.xcodeproj -scheme "RxWebKit iOS" -sdk iphonesimulator  -archivePath "xcarchives/RxWebKit-iOS-Simulator"
+xcodebuild archive -quiet -project RxWebKit-SPM.xcodeproj -scheme "RxWebKit macOS" -sdk macosx -archivePath "xcarchives/RxWebKit-macOS"
 
 xcodebuild -create-xcframework \
 -framework "xcarchives/RxWebKit-iOS-Simulator.xcarchive/Products/Library/Frameworks/RxWebKit.framework" \
